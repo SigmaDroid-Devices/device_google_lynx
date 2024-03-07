4163930ed31361752b05bd95253aa4185342dc95
Austin Young <austineyoung2000@gmail.com>
2024-03-07 18:25:11 -0500

    lynx: sigma upkeep

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,7 +17,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_lynx.mk \
     $(LOCAL_DIR)/factory_lynx.mk \
-    $(LOCAL_DIR)/lineage_lynx.mk
+    $(LOCAL_DIR)/sigma_lynx.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_lynx-userdebug
